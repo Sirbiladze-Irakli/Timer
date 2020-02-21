08aa7e59d04a6dad5cc939dd9ce4e4da3be36f9a
Irakli <9670451404@mail.ru>
2020-02-21 18:32:00 +0300

    Initial commit

---
+++ app/src/main/java/myapp/ru/timer/MainActivity.java
@@ -0,0 +1,14 @@
+package myapp.ru.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
